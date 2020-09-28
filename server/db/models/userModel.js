6daac5c24ea0e2b93c9261dcf6a5c505d558adef
Franco Trelles <franco.trellesm@gmail.com>
2020-09-27 20:04:00 -0400

    Incorporated login and signup controllers as well as protection middleware

--- server/db/models/userModel.js
+++ server/db/models/userModel.js
@@ -13,19 +13,25 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: [true, 'Email already in use'],
+    lowercase: true,
     validate: [validator.isEmail, 'Please provide a valid email'],
   },
   password: {
     type: String,
     required: [true, 'Please provide a password'],
+    minlength: 8,
     select: false,
   },
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm password'],
     select: false,
-    validate: function (val) {
-      return val === this.password;
+    validate: {
+      validator: function (val) {
+        return val === this.password;
+      },
+      message: 'Passwords are not the same!',
     },
   },
   profilePicture: {
@@ -53,6 +59,62 @@ const userSchema = new mongoose.Schema({
     select: false,
   },
 });
+
+//////////////////////////
+// PRE SAVE MIDDLEWARE //
+////////////////////////
+
+//Hashing password pre-save
+userSchema.pre('save', async function (next) {
+  //Only run this function is password was saved or modified
+  if (!this.isModified('password')) return next();
+
+  //Hash password with cost of 12 and set it as the password
+  this.password = await bcrypt.hash(this.password, 12);
+
+  this.passwordConfirm = undefined;
+  next();
+});
+
+//If password is changed, update passwordChangedAt
+userSchema.pre('save', function (next) {
+  if (!this.isModified('password') || this.isNew) return next();
+
+  this.passwordChangedAt = Date.now() - 1000;
+});
+
+//////////////////////////
+// PRE FIND MIDDLEWARE //
+////////////////////////
+
+//Hides inactive users from results
+userSchema.pre(/^find/, function (next) {
+  this.find({ activeAccount: { $ne: false } });
+  next();
+});
+
+//////////////////////////
+// INSTANCE METHODS /////
+////////////////////////
+
+//Compares inputted password and instance password
+userSchema.methods.correctPassword = async function (inputtedPass, userPass) {
+  return await bcrypt.compare(inputtedPass, userPass);
+};
+
+//Check if password was changed after being issued JWT
+userSchema.methods.changedPasswordAfter = function (timestampJWT) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+    //If password was changed after being issued JTW, return true
+    return timestampJWT < changedTimestamp;
+  }
+
+  return false;
+};
 
 const User = mongoose.model('User', userSchema);
 
--- server/routeControllers/authController.js
+++ server/routeControllers/authController.js
@@ -1,9 +1,48 @@
-const catchAsync = require('../utilities/catchAsync');
 const User = require('../db/models/userModel');
+const catchAsync = require('../utilities/catchAsync');
+const { promisify } = require('util');
+const jwt = require('jsonwebtoken');
 
+//CREATES TOKEN SIGNATURE
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
+//CREATE AND SEND TOKEN
+const createAndSendToken = (user, statusCode, res) => {
+  const token = signToken(user.id);
+
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  //Only secure in production
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
+  //Don't show password in response
+  user.password = undefined;
+
+  //Send back token and user
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user,
+    },
+  });
+};
+
+//CREATE NEW USER BY SIGNUP AND ASSIGN JWT TOKEN
 exports.signup = catchAsync(async (req, res, next) => {
   const { email, password, passwordConfirm, firstName, lastName } = req.body;
 
+  //Create user
   const newUser = await User.create({
     firstName,
     lastName,
@@ -12,8 +51,61 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm,
   });
 
-  res.status(201).json({
-    type: 'sucess',
-    data: newUser,
-  });
+  createAndSendToken(newUser, 201, res);
+});
+
+//LOGIN USER
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //Check if email and password are in req.body
+  if (!email || !password) {
+    return next(new Error('Please provide email and password'));
+  }
+
+  //Check if user exists
+  const user = await User.findOne({ email }).select('+password');
+
+  //Check if password is correct
+  if (!user || (await user.correctPassword(password, user.password))) {
+    return next(new Error('Incorrect email or password'));
+  }
+
+  createAndSendToken(user, 200, res);
+});
+
+//PROTECT MIDDLEWARE
+exports.protect = catchAsync(async (req, res, next) => {
+  //Check for token
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    //[0] = 'Bearer' and then grab the token at [1]
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  //If no token, not logged in
+  if (!token) return next(new Error('You are not logged in'));
+
+  //Validate token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  //Check if user still exists
+  const currentUser = await User.findById(decoded.id);
+
+  //If user does not exist return error
+  if (!currentUser) return next(new Error('This user no longer exists'));
+
+  //Check if user changed password after JWT issued
+  if (currentUser.changedPasswordAfter(decoded.iat))
+    return next(
+      new Error('User recently changed password. Please log in again')
+    );
+
+  //Add user to req
+  req.user = currentUser;
+  //Grant access to protected route
+  next();
 });
