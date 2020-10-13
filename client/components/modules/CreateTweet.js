48da278540ffa4a269cb47198d548ab413f637d7
Franco Trelles <franco.trellesm@gmail.com>
2020-10-13 03:36:27 -0400

    Converted CreateTweet class componented into a functional component

--- client/components/modules/CreateTweet.js
+++ client/components/modules/CreateTweet.js
@@ -1,38 +1,37 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useRef} from 'react'
 import {connect} from 'react-redux';
 import autosize from "autosize";
 import { fetchCreateTweet } from '../../store/tweets';
 
-class CreateTweet extends React.Component {
-  componentDidMount() {
-    this.textarea.focus();
-    autosize(this.textarea);
-  }
-  render() {
+const CreateTweet = ({handleSubmit}) => {
 
-    const handleSubmit = this.props.handleSubmit
+  const textarea = useRef(null)
 
-    return (
-      <>
-        <div className="tweet__profile-img"></div>
-        <div className="create-tweet">
-            <form onSubmit={handleSubmit}>
-                <textarea
-                className="create-tweet__text"
-                name="text"
-                ref={c => (this.textarea = c)}
-                placeholder="What's happening"
-                rows={1}
-                />
-                <div className="create-tweet__bottom">
-                    <svg viewBox="0 0 24 24" className="create-tweet__icon"><g><path d="M19.75 2H4.25C3.01 2 2 3.01 2 4.25v15.5C2 20.99 3.01 22 4.25 22h15.5c1.24 0 2.25-1.01 2.25-2.25V4.25C22 3.01 20.99 2 19.75 2zM4.25 3.5h15.5c.413 0 .75.337.75.75v9.676l-3.858-3.858c-.14-.14-.33-.22-.53-.22h-.003c-.2 0-.393.08-.532.224l-4.317 4.384-1.813-1.806c-.14-.14-.33-.22-.53-.22-.193-.03-.395.08-.535.227L3.5 17.642V4.25c0-.413.337-.75.75-.75zm-.744 16.28l5.418-5.534 6.282 6.254H4.25c-.402 0-.727-.322-.744-.72zm16.244.72h-2.42l-5.007-4.987 3.792-3.85 4.385 4.384v3.703c0 .413-.337.75-.75.75z"></path><circle cx="8.868" cy="8.309" r="1.542"></circle></g></svg>
-                    <button type="submit" className="btn btn--tweet">Tweet</button>
-                </div>
-            </form>
-        </div>
-      </>
-    );
-  }
+  useEffect(()=> {
+    textarea.current.focus()
+    autosize(textarea.current)
+  }, [])
+
+  return (
+          <>
+            <div className="tweet__profile-img"></div>
+            <div className="create-tweet">
+                <form onSubmit={handleSubmit}>
+                    <textarea
+                    className="create-tweet__text"
+                    name="text"
+                    ref={textarea}
+                    placeholder="What's happening"
+                    rows={1}
+                    />
+                    <div className="create-tweet__bottom">
+                        <svg viewBox="0 0 24 24" className="create-tweet__icon"><g><path d="M19.75 2H4.25C3.01 2 2 3.01 2 4.25v15.5C2 20.99 3.01 22 4.25 22h15.5c1.24 0 2.25-1.01 2.25-2.25V4.25C22 3.01 20.99 2 19.75 2zM4.25 3.5h15.5c.413 0 .75.337.75.75v9.676l-3.858-3.858c-.14-.14-.33-.22-.53-.22h-.003c-.2 0-.393.08-.532.224l-4.317 4.384-1.813-1.806c-.14-.14-.33-.22-.53-.22-.193-.03-.395.08-.535.227L3.5 17.642V4.25c0-.413.337-.75.75-.75zm-.744 16.28l5.418-5.534 6.282 6.254H4.25c-.402 0-.727-.322-.744-.72zm16.244.72h-2.42l-5.007-4.987 3.792-3.85 4.385 4.384v3.703c0 .413-.337.75-.75.75z"></path><circle cx="8.868" cy="8.309" r="1.542"></circle></g></svg>
+                        <button type="submit" className="btn btn--tweet">Tweet</button>
+                    </div>
+                </form>
+            </div>
+          </>
+        );
 }
 
 const mapDispatch = (dispatch, ownProps) => {
