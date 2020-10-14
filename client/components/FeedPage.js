4a001c96655d441452d4226fe1455c9c4024d642
Franco Trelles <franco.trellesm@gmail.com>
2020-10-14 19:21:39 -0400

    Optimized live search to only make api call once user stops typing

--- client/components/FeedPage.js
+++ client/components/FeedPage.js
@@ -1,65 +1,102 @@
-import React, {useEffect} from 'react';
+import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import { fetchDeleteTweet, fetchFeed } from '../store/tweets';
 import {CreateTweet, Tweet} from './modules/index'
 import {debounce} from 'lodash'
 import axios from 'axios'
 
-const FeedPage = ({location, feed, me, deleteTweet, getFeed}) => {
+class FeedPage extends Component {
 
-  useEffect(()=>{
-    getFeed(me.id)
-  }, [])
+  constructor(props){
+    super(props)
+    this.state = {
+      query: "",
+      results: [],
+      loading: false,
+      message: ""
+    }
+    this.cancel = ""
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.fetchResults = this.fetchResults.bind(this)
+  }
 
-  let cancelToken
+  componentDidMount() {
+    this.props.getFeed(this.props.me.id)
+  }
 
-  const handleSearchChange = debounce(async (text) => {
-    
+  async fetchResults(updatedPageNum = "", query){
+    const pageNum = updatedPageNum ? `&page=${updatedPageNum}` : "";
+
+    const url = `/api/users/search?username=${query}&page=${pageNum}&limit=4`
+
+    if (this.cancel) {
+      //cancel previous request before making a new request
+      this.cancel.cancel()
+    }
+
+   //Create cancel token
+   this.cancel = axios.CancelToken.source()
+
+   axios.get(url, {
+      cancelToken: this.cancel.token
+   }).then(res=> {
+      const noResults = !res.results ? "No results" : ""
+
+      this.setState({
+        results: res.data.data.users,
+        message: noResults,
+        loading: false
+      })
+
+   }).catch((err)=> {
+      if (axios.isCancel(err) || err) {
+
+        this.setState({
+          loading: false,
+          message: "Failed search. Please check network"
+        })
+      }
+   })
+
+}
+
+  handleInputChange = debounce((text) => {
     const query = text
 
-    //Check for previous request
-    if (typeof cancelToken !== typeof undefined) {
-      cancelToken.cancel("Canceled due to new request")
+    if (!query) {
+      this.setState({query, results: [], message: ""})
+    } else {
+      this.setState({query, loading: true, message: ""}, ()=> {
+        this.fetchResults(1, query)
+      })
     }
-
-    //Save the cancel token for current request
-    cancelToken = axios.CancelToken.source()
-
-    try {
-
-      const url = `/api/users/search?username=${query}&page=1&limit=4`
-
-      const {data} = await axios.get(url, { cancelToken: cancelToken.token })
-
-      console.log(query, data.data.users)
-    } catch (error) {
-      console.error(error)
-    }
-
-  }, 1000)
-
+  }, 1500)
   
-  return(
-        <div className="feed-page-container">
-          <nav className="secondary-nav">
-            <h3 className="nav-text util-margin-right-large">Home</h3>
-            <input onChange={(e) => handleSearchChange(e.target.value)} className="searchbar util-margin-auto-left util-margin-right-large" placeholder="Search Twitter"></input>
-          </nav>
-          <div className="feed-page-half style-scrollbars">
-             <div className="feed-middle">
-                  <div className="create-tweet-container">
-                    <CreateTweet location={location}/>
-                  </div>
-                  <div className="feed-container">
-                    {feed && feed.map(tweet => {
-                        return <Tweet key={tweet.id} tweet={tweet} me={me} deleteTweet={deleteTweet}/>
-                      })}
-                  </div>
-             </div>
-          </div>
-              
-        </div>)
+  render() {
 
+    const {location, feed, me, deleteTweet} = this.props
+
+    return(
+    <div className="feed-page-container">
+      <nav className="secondary-nav">
+        <h3 className="nav-text util-margin-right-large">Home</h3>
+        <input onChange={(e) => this.handleInputChange(e.target.value)} className="searchbar util-margin-auto-left util-margin-right-large" placeholder="Search Twitter"></input>
+      </nav>
+      <div className="feed-page-half style-scrollbars">
+         <div className="feed-middle">
+              <div className="create-tweet-container">
+                <CreateTweet location={location}/>
+              </div>
+              <div className="feed-container">
+                {feed && feed.map(tweet => {
+                    return <Tweet key={tweet.id} tweet={tweet} me={me} deleteTweet={deleteTweet}/>
+                  })}
+              </div>
+         </div>
+      </div>
+          
+    </div>)
+  }
 }
 
 const mapState = (state, ownProps) => {
