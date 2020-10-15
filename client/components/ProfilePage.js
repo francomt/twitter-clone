70e61f68b06aaac3ce8898fd2e4b48eca85b3e2e
Franco Trelles <franco.trellesm@gmail.com>
2020-10-15 16:17:44 -0400

    Back arrow on profile page

--- client/components/ProfilePage.js
+++ client/components/ProfilePage.js
@@ -40,6 +40,18 @@ const ProfilePage = ({
   return (
     <div className="profile-page-container">
       <nav className="secondary-nav">
+        <div
+          onClick={() => {
+            history.goBack();
+          }}
+          className="secondary-nav__back"
+        >
+          <svg className="secondary-nav__back__arrow" viewBox="0 0 24 24">
+            <g>
+              <path d="M20 11H7.414l4.293-4.293c.39-.39.39-1.023 0-1.414s-1.023-.39-1.414 0l-6 6c-.39.39-.39 1.023 0 1.414l6 6c.195.195.45.293.707.293s.512-.098.707-.293c.39-.39.39-1.023 0-1.414L7.414 13H20c.553 0 1-.447 1-1s-.447-1-1-1z"></path>
+            </g>
+          </svg>
+        </div>
         <h3 className="nav-text util-margin-right-large">{profile.name}</h3>
       </nav>
       <div
