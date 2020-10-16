b9494ec3672676fa866d2ee404ec026462719b05
Franco Trelles <franco.trellesm@gmail.com>
2020-10-16 00:32:27 -0400

    Styling for like

--- client/components/modules/Tweet.js
+++ client/components/modules/Tweet.js
@@ -1,16 +1,7 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef } from "react";
 import history from "../../history";
 
 const Tweet = ({ tweet, me, deleteTweet, likeTweet }) => {
-  // const [liked, setLiked] = useState(false);
-
-  // useEffect(() => {
-  //   const trueOrFalse = tweet.userLikes.some((user) => {
-  //     return user.id === me.id;
-  //   });
-
-  //   setLiked(trueOrFalse);
-  // }, []);
   const liked = tweet.userLikes.some((user) => {
     return user.id === me.id;
   });
@@ -144,7 +135,7 @@ const Tweet = ({ tweet, me, deleteTweet, likeTweet }) => {
           <div className="content__shares">
             {liked ? (
               <div
-                className="content__like-container liked"
+                className="content__like-container"
                 onClick={() => {
                   console.log("CLICKED");
                 }}
@@ -160,19 +151,19 @@ const Tweet = ({ tweet, me, deleteTweet, likeTweet }) => {
               </div>
             ) : (
               <div
-                className="content__like-container unliked"
+                className="content__like-container"
                 onClick={() => {
                   likeTweet(tweet.id);
                 }}
               >
                 <div className="content__icon-box">
-                  <svg viewBox="0 0 24 24" className="content__like">
+                  <svg viewBox="0 0 24 24" className="content__not-liked">
                     <g>
                       <path d="M12 21.638h-.014C9.403 21.59 1.95 14.856 1.95 8.478c0-3.064 2.525-5.754 5.403-5.754 2.29 0 3.83 1.58 4.646 2.73.814-1.148 2.354-2.73 4.645-2.73 2.88 0 5.404 2.69 5.404 5.755 0 6.376-7.454 13.11-10.037 13.157H12zM7.354 4.225c-2.08 0-3.903 1.988-3.903 4.255 0 5.74 7.034 11.596 8.55 11.658 1.518-.062 8.55-5.917 8.55-11.658 0-2.267-1.823-4.255-3.903-4.255-2.528 0-3.94 2.936-3.952 2.965-.23.562-1.156.562-1.387 0-.014-.03-1.425-2.965-3.954-2.965z"></path>
                     </g>
                   </svg>
                 </div>
-                <p className="content__like-num">{tweet.likes}</p>
+                <p className="content__not-like-num">{tweet.likes}</p>
               </div>
             )}
           </div>
