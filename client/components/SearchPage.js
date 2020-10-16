0446132d85bd3923650e90f3394f9575e6cbab3c
Franco Trelles <franco.trellesm@gmail.com>
2020-10-16 17:22:59 -0400

    Search for tweets almost done, need to add infinite scrolling

--- client/components/SearchPage.js
+++ client/components/SearchPage.js
@@ -1,9 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
-import { fetchSearchTweets } from "../store/results";
+import {
+  fetchLikeTweetSearch,
+  fetchSearchTweets,
+  fetchUnlikeTweetSearch,
+  fetchDeleteTweetSearch,
+} from "../store/results";
+import { fetchUpdatePrev } from "../store/tweets";
+import Tweet from "./modules/Tweet";
 import history from "../history";
 
-const SearchPage = ({ location, results, searchTweets }) => {
+const SearchPage = ({
+  location,
+  results,
+  searchTweets,
+  me,
+  likeTweet,
+  unlikeTweet,
+  deleteTweet,
+  updatePrev,
+}) => {
   const [query, setQuery] = useState("");
   const [type, setType] = useState("");
   const [input, setInput] = useState("");
@@ -15,19 +31,90 @@ const SearchPage = ({ location, results, searchTweets }) => {
 
     if (!q) return history.push("/explore");
 
-    const t = params.get("type");
+    //query type
+    const t = params.get("t");
 
     setQuery(q);
-    setType(t ? t : "tweet");
+    setType(t ? t : "latest");
 
     searchTweets(q);
+    updatePrev(history.location.pathname);
   }, []);
 
-  return <div className="search-page-container"></div>;
+  return (
+    <div className="search-page-container">
+      <div className="feed-middle">
+        <nav className="search-nav">
+          <div className="search-top">
+            <div
+              onClick={() => {
+                history.goBack();
+              }}
+              className="secondary-nav__back"
+            >
+              <svg className="secondary-nav__back__arrow" viewBox="0 0 24 24">
+                <g>
+                  <path d="M20 11H7.414l4.293-4.293c.39-.39.39-1.023 0-1.414s-1.023-.39-1.414 0l-6 6c-.39.39-.39 1.023 0 1.414l6 6c.195.195.45.293.707.293s.512-.098.707-.293c.39-.39.39-1.023 0-1.414L7.414 13H20c.553 0 1-.447 1-1s-.447-1-1-1z"></path>
+                </g>
+              </svg>
+            </div>
+            <form className="searchbar-container-page">
+              <input
+                className="searchbar"
+                placeholder="Search Twitter"
+                defaultValue={query}
+              ></input>
+              <svg viewBox="0 0 24 24" className="searchbar__icon-page">
+                <g>
+                  <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
+                </g>
+              </svg>
+            </form>
+          </div>
+          <div className="search-bottom">
+            <div
+              className={
+                type === "latest"
+                  ? "search-bottom__selection-active"
+                  : "search-bottom__selection"
+              }
+            >
+              <p className="search-bottom__text">Latest</p>
+            </div>
+            <div
+              className={
+                type === "people"
+                  ? "search-bottom__selection-active"
+                  : "search-bottom__selection"
+              }
+            >
+              <p className="search-bottom__text">People</p>
+            </div>
+          </div>
+        </nav>
+        <div className="results-list-container style-scrollbars">
+          {results.tweets.length > 0 &&
+            results.tweets.map((tweet) => {
+              return (
+                <Tweet
+                  key={tweet.id}
+                  me={me}
+                  tweet={tweet}
+                  likeTweet={likeTweet}
+                  unlikeTweet={unlikeTweet}
+                  deleteTweet={deleteTweet}
+                />
+              );
+            })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 const mapState = (state) => {
   return {
+    me: state.authReducer,
     results: state.searchReducer,
   };
 };
@@ -35,6 +122,12 @@ const mapState = (state) => {
 const mapDispatch = (dispatch) => {
   return {
     searchTweets: (query) => dispatch(fetchSearchTweets(query)),
+    likeTweet: (tweetId) => dispatch(fetchLikeTweetSearch(tweetId)),
+    unlikeTweet: (tweetId) => dispatch(fetchUnlikeTweetSearch(tweetId)),
+    deleteTweet: (tweetId) => dispatch(fetchDeleteTweetSearch(tweetId)),
+    updatePrev: (path) => {
+      dispatch(fetchUpdatePrev(path));
+    },
   };
 };
 
--- client/store/results.js
+++ client/store/results.js
@@ -3,16 +3,53 @@ import history from "../history";
 
 //ACTION TYPES
 const SEARCH_TWEETS = "SEARCH_TWEETS";
+const LIKE_TWEET_SEARCH = "LIKE_TWEET_SEARCH";
+const DELETE_TWEET_SEARCH = "DELETE_TWEET_SEARCH";
 
 const searchTweets = (tweets) => ({ type: SEARCH_TWEETS, tweets });
+const likeTweet = (tweet) => ({ type: LIKE_TWEET_SEARCH, tweet });
+const deleteTweet = (tweetId) => ({ type: DELETE_TWEET_SEARCH, tweetId });
 
 export const fetchSearchTweets = (query, page = 1) => {
   return async (dispatch) => {
     try {
       const { data } = await axios.get(
-        `/api/tweets/search?text=${query}&page=${page}&limit=5`
+        `/api/tweets/search?text=${query}&page=${page}&limit=30`
       );
       dispatch(searchTweets(data));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
+
+export const fetchLikeTweetSearch = (tweetId) => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.post(`/api/tweets/like/${tweetId}`);
+      dispatch(likeTweet(data));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
+
+export const fetchUnlikeTweetSearch = (tweetId) => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.post(`/api/tweets/unlike/${tweetId}`);
+      dispatch(likeTweet(data));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
+
+export const fetchDeleteTweetSearch = (tweetId) => {
+  return async (dispatch) => {
+    try {
+      await axios.delete(`/api/tweets/${tweetId}`);
+      dispatch(deleteTweet(tweetId));
     } catch (error) {
       console.error(error);
     }
@@ -39,6 +76,25 @@ function searchReducer(state = defaultState, action) {
       } else {
         return state;
       }
+
+    case LIKE_TWEET_SEARCH:
+      if (action.tweet.data) {
+        const updated = state.tweets.map((tweet) => {
+          if (tweet.id === action.tweet.data.tweet.id)
+            return action.tweet.data.tweet;
+          else return tweet;
+        });
+
+        return { ...state, tweets: updated };
+      } else {
+        return state;
+      }
+
+    case DELETE_TWEET_SEARCH:
+      const filtered = [...state.tweets].filter((tweet) => {
+        return tweet.id !== action.tweetId;
+      });
+      return { ...state, tweets: filtered };
 
     default:
       return state;
